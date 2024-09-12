fc8b59026ba9ee9e6703511fda0ab9e2001d5a3f
KelvinBecerra <urielbecerra39@gmail.com>
2024-09-12 13:11:34 -0400

    primer commit con alejandro

---
+++ PracticasAlejandro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicasalejandro;
+
+/**
+ *
+ * @author KelvinCi
+ */
+public class PracticasAlejandro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
